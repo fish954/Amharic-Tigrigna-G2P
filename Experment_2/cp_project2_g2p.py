50b79fd56a996e9fc2b1a28c06887ef29fc83781
dagi931 <dagiderbe59@gmail.com>
2025-05-28 15:35:14 +0300

    Update cp_project2_g2p.py

--- Experment_2/cp_project2_g2p.py
+++ Experment_2/cp_project2_g2p.py
@@ -88,3 +88,7 @@ def g2p(text: str):
     for char in text:
         phonemes.append(g2p_mapping.get(char, char))
     return ' '.join(phonemes)  # Use space to preserve phoneme boundaries
+
+# Count full phoneme units
+def phoneme_distribution(phoneme_str: str):
+    return Counter(phoneme_str.split())
